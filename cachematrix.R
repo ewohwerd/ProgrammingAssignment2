24972babf90d37e4c29a1c5268b8e1e689e0b3ed
Andrew Howe <drewhowe@gmail.com>
2015-07-22 13:26:33 -0400

    Built out makeCacheMatrix and cacheSolve. Added comments. Used suggested example code from redpeng's README that stores and retrieves vector mean() results and modified to work with solve() and matrices

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caches matrix inverse solutions
+## Run makeCacheMatrix on matrix you wish to invert, then cacheSolve on the result of makeCacheMatrix
+## cacheSolve will only run solve() if no cache exists for given matrix
 
-## Write a short comment describing this function
-
+## Creates a special object that can be used with cacheSolve to store and or retrieve solve() results for
+## provided matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setsolve <- function(solve) m <<- solve
+      getsolve <- function() m
+      list(set = set, get = get,
+           setsolve = setsolve,
+           getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## when run on a makeCacheMatrix result, checks for a stored inverse, returns it if the matrix exists, or
+## alternatively runs solve() and stores the result
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      m <- x$getsolve()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setsolve(m)
+      m
 }
